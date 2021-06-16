ffee8f12d04ec275445cd64bfa7350aae983da13
Leonel Aguirre <leonel.aguirre@hbr.org>
2021-06-16 16:58:08 -0500

    :memo: Comments added.

--- src/main/java/com/noisyapple/App.java
+++ src/main/java/com/noisyapple/App.java
@@ -5,6 +5,7 @@ public class App {
 
         if (args.length > 0) {
 
+            // Takes the path of the file from args.
             String fileData = ExternalFileReader.fileToString(args[0]);
 
             LexicalAnalyzer lA = new LexicalAnalyzer(fileData);
