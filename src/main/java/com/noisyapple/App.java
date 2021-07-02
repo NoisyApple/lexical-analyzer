88721fef112fcef253cd7a6769721a1b78d766f6
Leonel Aguirre <leonel.aguirre@hbr.org>
2021-07-01 20:22:38 -0500

    :recycle: getGrammarParts functionality moved to Grammar class; epsilon excluded from terminal symbols.

--- src/main/java/com/noisyapple/App.java
+++ src/main/java/com/noisyapple/App.java
@@ -1,7 +1,5 @@
 package com.noisyapple;
 
-import java.util.Arrays;
-
 public class App {
     public static void main(String[] args) {
 
@@ -16,54 +14,9 @@ public class App {
             lA.startAnalysis();
             System.out.println(lA.toString());
 
-            String[][] grammarParts = Utils.getGrammarParts(grammarFile);
+            Grammar g = new Grammar(grammarFile);
 
-            String[] nonTerminalSymbols = grammarParts[0];
-            String[] terminalSymbols = grammarParts[1];
-            String[] rulesRightSides = grammarParts[2];
-
-            String grammarData = "";
-
-            grammarData += "------------------[FILE]-------------------\n";
-            grammarData += grammarFile + "\n\n";
-
-            grammarData += "--------------[NON TERMINALS]--------------\n";
-            for (String s : nonTerminalSymbols)
-                grammarData += s + "\n";
-
-            grammarData += "\n\n----------------[TERMINALS]----------------\n";
-            for (String s : terminalSymbols)
-                grammarData += s + "\n";
-
-            grammarData += "\n\n---------------[RIGHT SIDES]---------------\n";
-            for (String s : rulesRightSides)
-                grammarData += s + "\n";
-
-            System.out.println("\n\n" + grammarData);
-
-            Stack<String> stringsStack = new Stack<String>();
-
-            System.out.println(stringsStack.size()); // 0
-
-            System.out.println(stringsStack.isEmpty()); // true
-
-            stringsStack.push("A"); // ["A"]
-            stringsStack.push("B"); // ["A", "B"]
-            stringsStack.push("C"); // ["A", "B", "C"]
-
-            System.out.println(stringsStack); // ["A", "B", "C"]
-
-            System.out.println(stringsStack.pop()); // "C"
-
-            System.out.println(stringsStack); // ["A", "B"];
-
-            System.out.println(stringsStack.peek()); // "B"
-
-            System.out.println(stringsStack); // ["A", "B"];
-
-            System.out.println(stringsStack.size()); // 2
-
-            System.out.println(stringsStack.isEmpty()); // false
+            System.out.println(g.toString());
 
         } else {
             throw new Error("No input files paths specified.");
---
+++ src/main/java/com/noisyapple/Grammar.java
@@ -0,0 +1,80 @@
+package com.noisyapple;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+// Contains the program grammar.
+public class Grammar {
+
+    private String[] productionRules;
+    private String[] nonTerminalSymbols;
+    private String[] terminalSymbols;
+    private String[] rulesRightSides;
+
+    // Class constructor.
+    public Grammar(String grammar) {
+
+        String[] productionRules = grammar.split("\n");
+
+        ArrayList<String> nonTerminalSymbols = new ArrayList<String>();
+        ArrayList<String> terminalSymbols = new ArrayList<String>();
+        ArrayList<String> rulesRightSides = new ArrayList<String>();
+
+        Arrays.stream(productionRules).forEach(pR -> {
+            String[] ruleSides = pR.split(" -> ");
+
+            if (ruleSides.length != 2) {
+                throw new Error("Inconsistent production rule found.");
+            }
+
+            if (!nonTerminalSymbols.contains(ruleSides[0])) {
+                nonTerminalSymbols.add(ruleSides[0]);
+            }
+
+            rulesRightSides.add(ruleSides[1]);
+        });
+
+        rulesRightSides.forEach(rS -> {
+            String[] symbols = rS.split(" ");
+
+            for (String s : symbols) {
+                if (!terminalSymbols.contains(s) && !nonTerminalSymbols.contains(s)
+                        && !s.equals("ε")) {
+                    terminalSymbols.add(s);
+                }
+            }
+        });
+
+        this.productionRules = productionRules;
+        this.nonTerminalSymbols = nonTerminalSymbols.toArray(new String[0]);
+        this.terminalSymbols = terminalSymbols.toArray(new String[0]);
+        this.rulesRightSides = rulesRightSides.toArray(new String[0]);
+
+    }
+
+    public String toString() {
+        String grammarData = "";
+
+        grammarData += "------------[PRODUCTION RULES]-------------\n";
+        for (String s : productionRules)
+            grammarData += s + "\n";
+
+        grammarData += "\n\n--------------[NON TERMINALS]--------------\n";
+        for (String s : nonTerminalSymbols)
+            grammarData += s + "\n";
+
+        grammarData += "\n\n----------------[TERMINALS]----------------\n";
+        for (String s : terminalSymbols)
+            grammarData += s + "\n";
+
+        grammarData += "\n\n---------------[RIGHT SIDES]---------------\n";
+        for (String s : rulesRightSides)
+            grammarData += s + "\n";
+
+        System.out.println("Hey There!");
+
+
+        return "\n\n" + grammarData;
+    }
+
+}
